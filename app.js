4f8a1dac21564f03a53bd8785f301ddb0358e511
Imal Kumarage <imalkrulez@gmail.com>
2022-01-05 20:59:15 +0530

    Added MongoDB connection

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+
+//Load config
+dotenv.config({ path: './config/config.env' });
+
+connectDB();
+
+const app = express();
+
+// Logging
+if ((process.env.NODE_ENV = 'development')) {
+  app.use(morgan('dev'));
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server running on ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
