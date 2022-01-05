af9d5653590954f8d72227de0f50b6b14daffd85
Imal Kumarage <imalkrulez@gmail.com>
2022-01-05 21:35:42 +0530

    Added routing for handlebars

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const morgan = require('morgan');
-const exphbs = require('express-handlebars');
+// const { engine } = require('express-handlebars');
+var exphbs = require('express-handlebars');
+
 //Load config
 dotenv.config({ path: './config/config.env' });
 
@@ -16,12 +18,14 @@ if ((process.env.NODE_ENV = 'development')) {
 }
 
 // Handlebars
-app.engine(
-  'handlebars',
-  exphbs.engine({ defaultLayout: 'main', extname: '.hbs' })
-);
+// app.engine('handlebars', engine({ extname: '.hbs', defaultLayout: 'main' }));
+// app.set('view engine', '.hbs');
+// app.set('views', './views');
+app.engine('.hbs', exphbs.engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
-app.set('views', './views');
+
+//Routes
+app.use('/', require('./routes/index'));
 
 const PORT = process.env.PORT || 5000;
 
